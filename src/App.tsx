c82470cfefef2b898f498c5f6352167621b20e00
nanidam <nani.dam@medieinstitutet.se>
2023-01-29 00:00:30 +0100

    :wrench: fixed named files

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { Router, Route, BaseLocationHook } from 'wouter'
-import { useLocationProperty, navigate } from 'wouter/use-location'
-import { Table } from './components/Table'
-import { About } from './routes/About'
-import { Home } from './routes/Home'
-import { Login } from './routes/RouteLogin'
-import { RouteRegister } from './routes/RouteRegister'
+import React from "react";
+import { Router, Route, BaseLocationHook } from "wouter";
+import { useLocationProperty, navigate } from "wouter/use-location";
+import { Table } from "./components/LobbyTable";
+import { About } from "./routes/About";
+import { Home } from "./routes/Home";
+import { Login } from "./routes/RouteLogin";
+import { RouteRegister } from "./routes/RouteRegister";
 
-const hashLocation = () => window.location.hash.replace(/^#/, '') || '/'
-const hashNavigate = (to: string) => navigate(`#${to}`)
+const hashLocation = () => window.location.hash.replace(/^#/, "") || "/";
+const hashNavigate = (to: string) => navigate(`#${to}`);
 const useHashLocation: BaseLocationHook = () => {
-  const location = useLocationProperty(hashLocation)
-  return [location, hashNavigate]
-}
+  const location = useLocationProperty(hashLocation);
+  return [location, hashNavigate];
+};
 
 export const App: React.FunctionComponent = () => (
   <Router hook={useHashLocation}>
@@ -21,7 +21,7 @@ export const App: React.FunctionComponent = () => (
     <Route path="/register" component={RouteRegister} />
     <Route path="/about" component={About} />
     <Route path="/table/:tableID">
-      {({ tableID }) => <Table tableID={parseInt(tableID || '0', 10)} />}
+      {({ tableID }) => <Table tableID={parseInt(tableID || "0", 10)} />}
     </Route>
   </Router>
-)
+);
