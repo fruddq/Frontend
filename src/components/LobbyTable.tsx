ad523c6750ac373e99e8cff8a75ce7dd762a5ab9
nanidam <nani.dam@medieinstitutet.se>
2023-01-29 01:30:25 +0100

    :muscle: added popup +bg for start/leaving game

--- src/components/LobbyTable.tsx
+++ src/components/LobbyTable.tsx
@@ -6,9 +6,23 @@ export const Table: React.FunctionComponent<{
   tableID: number;
 }> = ({ tableID }) => (
   <div>
+    <div className="bg-popups hidden"></div>
     <GameTitle />
+    <div className="lobby-popup-starting-game hidden">
+      <p className="lobby-popup-starting-text">
+        Game starting...
+        <br />
+        Please rotate your phone.
+      </p>
+    </div>
+    <div className="lobby-popup-leaving-game hidden">
+      <p className="lobby-popup-leaving-text">
+        Are you sure you want to leave?
+      </p>
+      <button className="lobby-leave-btn lobby-leave-yes-btn">Yes</button>
+      <button className="lobby-leave-btn lobby-leave-no-btn">No</button>
+    </div>
     <nav className="lobby-loggedin-player">Logged in as: *PLAYER* </nav>
-
     <h2 className="lobby-game-name">This is Table:{tableID}</h2>
     <section className="lobby-game-table">
       <button className="lobby-seat">Empty seat</button>
